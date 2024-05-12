84d1c834d57fad61f3c1f6f5e19b69c998dad2a5
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-05-12 10:26:24 +0200

    Change the place for testing libs mdlp, catch2 and File Add copyright headers to source files

--- tests/TestFolding.cc
+++ tests/TestFolding.cc
@@ -1,3 +1,9 @@
+// ***************************************************************
+// SPDX-FileCopyrightText: Copyright 2024 Ricardo Montañana Gómez
+// SPDX-FileType: SOURCE
+// SPDX-License-Identifier: MIT
+// ***************************************************************
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
 #include <catch2/generators/catch_generators.hpp>
--- tests/lib/Files/ArffFiles.h
+++ tests/lib/Files/ArffFiles.h
@@ -1,3 +1,9 @@
+// ***************************************************************
+// SPDX-FileCopyrightText: Copyright 2024 Ricardo Montañana Gómez
+// SPDX-FileType: SOURCE
+// SPDX-License-Identifier: MIT
+// ***************************************************************
+
 #ifndef ARFFFILES_H
 #define ARFFFILES_H
 
@@ -5,15 +11,6 @@
 #include <vector>
 
 class ArffFiles {
-private:
-    std::vector<std::string> lines;
-    std::vector<std::pair<std::string, std::string>> attributes;
-    std::string className;
-    std::string classType;
-    std::vector<std::vector<float>> X;
-    std::vector<int> y;
-    void generateDataset(int);
-    void loadCommon(std::string);
 public:
     ArffFiles();
     void load(const std::string&, bool = true);
@@ -27,6 +24,15 @@ public:
     std::vector<int>& getY();
     std::vector<std::pair<std::string, std::string>> getAttributes() const;
     static std::vector<int> factorize(const std::vector<std::string>& labels_t);
+private:
+    std::vector<std::string> lines;
+    std::vector<std::pair<std::string, std::string>> attributes;
+    std::string className;
+    std::string classType;
+    std::vector<std::vector<float>> X;
+    std::vector<int> y;
+    void generateDataset(int);
+    void loadCommon(std::string);
 };
 
 #endif
